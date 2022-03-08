834bcb203bc8091ff5806be5b33b4034d9f39b0c
Cheng Wuai <jaysoncheng999@gmail.com>
2022-03-08 18:28:25 +0800

    Added data collection section code at line 10 for cloud,py file

--- cloud.py
+++ cloud.py
@@ -2,9 +2,15 @@ from requests_html import HTMLSession
 
 s = HTMLSession()
 
-query = 'malaysia'
+query = ''
 url = f'https://www.google.com/search?q=weather+{query}'
 
 r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'})
 
-print(r.html.find('title', first=True).text)
+temp = r.html.find('span#wob_tm', first=True).text
+
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+print(query, temp, unit, desc)
