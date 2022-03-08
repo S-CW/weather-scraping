a60af0e5dc1cd956d6b71c1c453931d00f8f9a1c
Cheng Wuai <jaysoncheng999@gmail.com>
2022-03-08 13:53:07 +0800

    Added cloud.py, requirements.txt, and .gitignore files

---
+++ cloud.py
@@ -0,0 +1,10 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+
+query = 'malaysia'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'})
+
+print(r.html.find('title', first=True).text)
